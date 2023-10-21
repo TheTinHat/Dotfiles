448e0eae0102b075695c1f6022c43e20fde4503a
David Swanlund <10473778+TheTinHat@users.noreply.github.com>
2023-10-21 13:17:20 -0700

    Add --delete flag to Rsync

--- nixos/roles/backup_home_daily.nix
+++ nixos/roles/backup_home_daily.nix
@@ -1,11 +1,11 @@
-{ pkgs, ...}: {
+{ pkgs, ... }: {
   # RSync backup job
   systemd.services."rsync-backup" = {
     path = [ pkgs.rsync pkgs.openssh ];
     script = ''
       BACKUP_PATH="/mnt/rust/archives/backups/nix-homes/$HOSTNAME/"
-      rsync -avR --mkpath /home/ admin@192.168.1.200:$BACKUP_PATH
-      '';
+      rsync -avR --delete --mkpath /home/ admin@192.168.1.200:$BACKUP_PATH
+    '';
     serviceConfig = {
       Type = "oneshot";
       User = "root";
@@ -19,5 +19,5 @@
       OnCalendar = "hourly";
       Persistent = true;
     };
-  }; 
+  };
 }
