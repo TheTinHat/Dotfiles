18db12fc748a3740b0604790188cc12478f4575e
David Swanlund <10473778+TheTinHat@users.noreply.github.com>
2023-10-21 12:44:22 -0700

    Add flake support

--- nixos/roles/standard_desktop.nix
+++ nixos/roles/standard_desktop.nix
@@ -1,33 +1,35 @@
-{ pkgs, ...}:
+{ pkgs, ... }:
 {
   imports = [
-    ../pkgs_override.nix 
+    ../pkgs_override.nix
   ];
 
   environment.systemPackages = with pkgs; [
-      flatpak
-      gnome.gnome-tweaks
-      element-desktop
-      bitwarden
-      firefox
-      qgis
-      libreoffice
-      tor-browser-bundle-bin
-      chromium
-      vscodium
-      steam
-      inkscape
-      gimp
-      virt-manager
-      wireshark
-      vlc
-      syncthing
-      
-      # Unstable
-      unstable.prusa-slicer
-      unstable.obsidian
-      unstable.darktable
+    flatpak
+    gnome.gnome-tweaks
+    element-desktop
+    bitwarden
+    firefox
+    qgis
+    libreoffice
+    tor-browser-bundle-bin
+    chromium
+    vscodium
+    steam
+    inkscape
+    gimp
+    virt-manager
+    wireshark
+    vlc
+    syncthing
+
+    # Unstable
+    unstable.prusa-slicer
+    unstable.obsidian
+    unstable.darktable
   ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   programs.steam.enable = true;
 
